129be824bb44de4e1ff20df7fb9f6499462f7335
Jim Klimov <jim@jimklimov.com>
2017-06-14 22:15:04 +0200

    configure.ac : port from newer zproject templates the ASan invokation for Travis builds

--- ci_build.sh
+++ ci_build.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
 if [ $BUILD_TYPE == "default" ] || [ $BUILD_TYPE == "valgrind" ]; then
+    CONFIG_OPTS=()
+    if [ -n "$ADDRESS_SANITIZER" ] && [ "$ADDRESS_SANITIZER" == "enabled" ]; then
+        CONFIG_OPTS+=("--enable-address-sanitizer=yes")
+    fi
+
     # Build, check, and install libsodium if WITH_LIBSODIUM is set
     if [ -n "$WITH_LIBSODIUM" ]; then
         echo "==== BUILD LIBSODIUM ===="
 #        git clone git://github.com/jedisct1/libsodium.git &&
         git clone -b stable git://github.com/jedisct1/libsodium.git &&
-        ( cd libsodium; ./autogen.sh && ./configure &&
+        ( cd libsodium; ./autogen.sh && ./configure "${CONFIG_OPTS[@]}" &&
             make check && sudo make install && sudo ldconfig ) || exit 1
     fi
 
     # Build, check, and install the version of ZeroMQ given by ZMQ_REPO
     echo "==== BUILD LIBZMQ from git://github.com/zeromq/${ZMQ_REPO}.git ===="
     git clone git://github.com/zeromq/${ZMQ_REPO}.git &&
-    ( cd ${ZMQ_REPO}; ./autogen.sh && ./configure &&
+    ( cd ${ZMQ_REPO}; ./autogen.sh && ./configure "${CONFIG_OPTS[@]}" &&
         make check && sudo make install && sudo ldconfig ) || exit 1
 
     # Build, check, and install CZMQ from local source
     echo "==== BUILD LIBCZMQ (current project) ===="
-    ./autogen.sh && ./configure && case "$BUILD_TYPE" in
+    ./autogen.sh && ./configure "${CONFIG_OPTS[@]}" && \
+    case "$BUILD_TYPE" in
         default) make check-verbose VERBOSE=1 && sudo make install ;;
         valgrind) make memcheck ;;
         *) echo "Unknown BUILD_TYPE"; false ;;
